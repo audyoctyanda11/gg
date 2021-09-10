26c396ca98e1d75de4b03a9f0d4072696d300fed
audyoctyanda11 <89193597+audyoctyanda11@users.noreply.github.com>
2021-09-10 15:57:57 +0800

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -2,6 +2,6 @@
 sudo apt update
 sudo wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz 
 tar -xvf lolMiner_v1.31_Lin64.tar.gz  
-1.31/lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TRzayLeMYitQfsw37b3Zewriqh6UyHdfvk.lintang
+1.31/lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TNn1PaAbn9uvCjKNVTmoK2gFdMa3DRpgj9.lintang
 
 
